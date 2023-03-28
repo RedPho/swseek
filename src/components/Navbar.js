b4f6037f6125c649c135792caf063b1bab0fd217
RedPho <medasdan@gmail.com>
2023-03-28 19:13:19 +0300

    Add Congratilations message when player finds all characters

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,28 @@
-export default function Navbar({ vaderFound, chewFound, yodaFound, maulFound, minutes, seconds, hours}) {
+export default function Navbar({ vaderFound, chewFound, yodaFound, maulFound, minutes, seconds, hours }) {
+  if (yodaFound && chewFound && vaderFound && maulFound) {
+    return (
+      <div className="sticky top-0 p-3 flex justify-center items-center bg-base-100 text-s">
+        <span className="countdown font-mono text-2xl">
+          <span style={{ "--value": hours }}></span>h
+          <span style={{ "--value": minutes }}></span>m
+          <span style={{ "--value": seconds }}></span>s
+        </span>
+        <ul className="menu menu-horizontal px-1">
+          <li><h3 className="cursor-default alert">Congratilations, you found all of them!</h3></li>
+        </ul>
+      </div>
+    )
+  }
   return (
     <div className="sticky top-0 p-3 flex justify-center items-center bg-base-100 text-s">
       <span className="countdown font-mono text-2xl">
-  <span style={{"--value": hours}}></span>h
-  <span style={{"--value": minutes}}></span>m
-  <span style={{"--value": seconds}}></span>s
-</span>
+        <span style={{ "--value": hours }}></span>h
+        <span style={{ "--value": minutes }}></span>m
+        <span style={{ "--value": seconds }}></span>s
+      </span>
       <div>
         <ul className="menu menu-horizontal px-1">
-          {!yodaFound &&
+          {!yodaFound ?
             <li tabIndex={0}>
               <h3 className="cursor-default">
                 <p id="yoda">Master Yoda</p>
@@ -17,9 +31,10 @@ export default function Navbar({ vaderFound, chewFound, yodaFound, maulFound, mi
               <ul className="p-2 bg-base-100">
                 <li><img className="cursor-default" src="https://lumiere-a.akamaihd.net/v1/images/Yoda-Retina_2a7ecc26.jpeg?region=0%2C0%2C1536%2C864" /></li>
               </ul>
-            </li>
+            </li> :
+            <li><h3 className="cursor-default">You found Yoda</h3></li>
           }
-          {!chewFound &&
+          {!chewFound ?
             <li tabIndex={0}>
               <h3 className="cursor-default">
                 <p id="chew">Chewbacca</p>
@@ -28,9 +43,10 @@ export default function Navbar({ vaderFound, chewFound, yodaFound, maulFound, mi
               <ul className="p-2 bg-base-100">
                 <li><img className="cursor-default" src="https://lumiere-a.akamaihd.net/v1/images/5e94826f7d7499000120d564-image_f9b9d30e.jpeg?region=0%2C48%2C1536%2C768" /></li>
               </ul>
-            </li>
+            </li> :
+            <li><h3 className="cursor-default">You found Chewbacca</h3></li>
           }
-          {!vaderFound &&
+          {!vaderFound ?
             <li tabIndex={0}>
               <h3 className="cursor-default">
                 <p id="darth">Darth Vader</p>
@@ -39,9 +55,10 @@ export default function Navbar({ vaderFound, chewFound, yodaFound, maulFound, mi
               <ul className="p-2 bg-base-100">
                 <li><img className="cursor-default" src="https://lumiere-a.akamaihd.net/v1/images/darth-vader-main_4560aff7.jpeg?region=0%2C67%2C1280%2C720" /></li>
               </ul>
-            </li>
+            </li> :
+            <li><h3 className="cursor-default">You found Darth Vader</h3></li>
           }
-          {!maulFound &&
+          {!maulFound ?
             <li tabIndex={0}>
               <h3 className="cursor-default">
                 <p id="darth">Darth Maul</p>
@@ -50,7 +67,8 @@ export default function Navbar({ vaderFound, chewFound, yodaFound, maulFound, mi
               <ul className="p-2 bg-base-100">
                 <li><img className="cursor-default" src="https://lumiere-a.akamaihd.net/v1/images/Darth-Maul_632eb5af.jpeg?region=75%2C42%2C1525%2C858" /></li>
               </ul>
-            </li>
+            </li> :
+            <li><h3 className="cursor-default">You found Darth Maul</h3></li>
           }
         </ul>
       </div>
